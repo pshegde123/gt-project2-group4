3a2478f240491ff000e8afd5a6cb7449ab9abdb6
pradnya <pradnyahegde@gmail.com>
2019-12-07 23:23:39 -0500

    added post method for /api/bookmark route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,8 +3,16 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.BookMark.findAll({}).then(function(dbExamples) {
       res.render("index", {
+        examples: dbExamples
+      });
+    });
+  });
+  // Load bookmarks page
+  app.get("/Bookmarks", function(req, res) {
+    db.BookMark.findAll({}).then(function(dbExamples) {
+      res.render("bookmark", {
         msg: "Welcome!",
         examples: dbExamples
       });
@@ -13,7 +21,7 @@ module.exports = function(app) {
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.BookMark.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
       res.render("example", {
         example: dbExample
       });
@@ -24,4 +32,17 @@ module.exports = function(app) {
   app.get("*", function(req, res) {
     res.render("404");
   });
+
+    // POST route for saving a new bookmark
+  app.post("/api/bookmark", function(req, res) {
+      db.Bookmark.create({
+        City: req.body.city,
+        Address: req.body.address,
+        Notes: req.body.notes
+      })
+        .then(function(result) {
+          res.json(result);
+        });
+    });
+  
 };
