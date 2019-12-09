57c7cdab8a56b3a00b9625775e2a57d00f7c0b7f
pradnya <pradnyahegde@gmail.com>
2019-12-09 11:42:10 -0500

    added route delete(/api/bookmarks:id)

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -19,9 +19,9 @@ module.exports = function(app) {
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  // Delete a Bookmark by id
+  app.delete("/api/bookmark/:id", function(req, res) {
+    db.BookMark.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
       res.json(dbExample);
     });
   });
