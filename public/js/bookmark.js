5bbd736b81a46fae9cbb7230faf79077b034d7eb
pradnya <pradnyahegde@gmail.com>
2019-12-09 11:41:26 -0500

    added delete button handeler

--- public/js/bookmark.js
+++ public/js/bookmark.js
@@ -2,6 +2,7 @@
 $(function () {
     // Retrieve DOM elements and store them
     var contactForm = $('#bookMarkEntryForm');
+
     contactForm.submit(function (e) {
         e.preventDefault();
         
@@ -24,4 +25,17 @@ $(function () {
             }
         );
     });  
+
+    $(".deleteBookmark").on("click", function(event) {
+        var id = $(this).data("id");
+        // Send the DELETE request.
+        $.ajax("/api/bookmark/" + id, {
+          type: "DELETE"
+        }).then(
+          function() {
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+      });
 });    
