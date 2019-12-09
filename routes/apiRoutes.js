141991b1abd09e691facc331271e1933c02feeb7
pradnya <pradnyahegde@gmail.com>
2019-12-08 21:13:01 -0500

    added route /api/bookmarks/add

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,14 +2,14 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/api/read", function(req, res) {
+    db.BookMark.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
     });
   });
 
   // Create a new example
-  app.post("/add", function(req, res) {
+  app.post("/api/bookmarks/add", function(req, res) {
     db.BookMark.create({
       City:req.body.city,
       Address:req.body.address,
