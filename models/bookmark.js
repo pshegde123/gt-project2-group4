0e207b29ac181fbba8753e34d8b56c0454d97d15
pradnya <pradnyahegde@gmail.com>
2019-12-12 15:45:28 -0500

    removed column Image

--- models/bookmark.js
+++ models/bookmark.js
@@ -2,8 +2,7 @@ module.exports = function(sequelize, DataTypes) {
   var BookMark = sequelize.define("BookMark", {
     City: { type:DataTypes.STRING, allowNull: false},
     Address: { type:DataTypes.TEXT, allowNull: false},
-    Notes:DataTypes.TEXT,
-    Image: { type:DataTypes.STRING, allowNull: true,defaultValue:'https://cdn2.momjunction.com/wp-content/uploads/2014/10/halloween-coloring-pages-printable-scary-a4.jpg'}
+    Notes:DataTypes.TEXT
   },{
       timestamps: false
   });
